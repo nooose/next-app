fbd3d5baa5af785539668ab4b7fdddaf14d52ec6
nooose <hd15807@gmail.com>
2024-03-18 22:49:57 +0900

    feat: 간단한 컴포넌트 구성

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,7 +1,11 @@
 import Avatar from "./company/components/avatar";
+import Navigation from "../../components/navigation";
 
 export default function AboutUs() {
-    return <header>
+    return (
+        <header>
+            <Navigation />
             <h1>About us! <Avatar/></h1>
         </header>
+    )
 }

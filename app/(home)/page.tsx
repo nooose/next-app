a91bd1aac758b5005278bc31cc18f372286e0e6d
nooose <hd15807@gmail.com>
2024-04-11 22:56:59 +0900

    feat: use clinet fetch 사용

---
+++ app/(home)/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState();
+  const getMovies = async () => {
+    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  }
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return ( 
+      <div>
+        {isLoading ? "Loading..." : JSON.stringify(movies)}
+      </div>
+  )
+}
